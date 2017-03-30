f968e915e6dbffa4f270fe1d3049e80d0203c434
Faustino Aguilar <faustinoaq@gmail.com>
2017-03-30 06:48:47 -0500

    Add default files

--- src/sentry-run/sentry-config.cr
+++ src/sentry-run/sentry-config.cr
@@ -7,7 +7,7 @@ module Sentry
                   run_command : String,
                   build_args = [] of String,
                   run_args = [] of String,
-                  files = [] of String,
+                  files = ["src/**/*.cr", "src/**/*.ecr"],
                   should_build = true)
     Sentry::ProcessRunner.new(
       process_name,
