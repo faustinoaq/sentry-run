c1c6320717614507a9e5142783e749440c32ec66
Faustino Aguilar <faustinoaq@gmail.com>
2017-03-30 02:54:19 -0500

    Fix spec

--- spec/sentry-run_spec.cr
+++ spec/sentry-run_spec.cr
@@ -2,14 +2,14 @@ require "./spec_helper"
 
 describe Sentry do
   it "create sentry.lock" do
-    File.write(Sentry::Config::INSTANCE.lock, "")
-    exists? = File.exists? Sentry::Config::INSTANCE.lock
+    File.write(Sentry::LOCK, "")
+    exists? = File.exists? Sentry::LOCK
     exists?.should eq(true)
   end
 
   it "delete sentry.lock" do
-    File.delete Sentry::Config::INSTANCE.lock
-    exists? = File.exists? Sentry::Config::INSTANCE.lock
+    File.delete Sentry::LOCK
+    exists? = File.exists? Sentry::LOCK
     exists?.should eq(false)
   end
 end
